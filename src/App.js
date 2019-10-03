37e78f9538d58da2a47a45d6d60cbe30576f210d
Peter Nelson <Peter.Nelson@bestbuy.com>
2019-10-02 19:15:33 -0500

    Add basic validation, sign up button flips the form, no confirmation content yet

--- src/App.js
+++ src/App.js
@@ -7,14 +7,17 @@ class App extends React.Component {
     this.state = {
       firstName: '',
       emailAddress: '',
-      password: ''
+      password: '',
+      shouldShowConfirmation: false
     }
 
     this.updateFirstName = this.updateFirstName.bind(this);
     this.updateEmailAddress = this.updateEmailAddress.bind(this);
     this.updatePassword = this.updatePassword.bind(this);
     this.signUp = this.signUp.bind(this);
-    this.validateFields = this.validateFields.bind(this);
+    this.validateFields = this.isFormValid.bind(this);
+    this.getFormContent = this.getFormContent.bind(this);
+    this.getConfirmationContent = this.getConfirmationContent.bind(this);
   }
 
   updateFirstName(event) {
@@ -30,30 +33,51 @@ class App extends React.Component {
   }
 
   signUp() {
-    this.validateFields();
-    // flip to the welcome screen
+    if (!this.isFormValid()) return;
+    this.setState({ shouldShowConfirmation: true });
   }
 
-  validateFields() {
-    // all three fields are required to submit
+  isFormValid() {
+    const { firstName, emailAddress, password} = this.state;
+    let isValid = true;
+
+    // For brevity, only checking for truthiness i.e. not empty strings
+    if (!firstName.trim()) { alert('First name is required'); isValid = false; }
+    else if (!emailAddress.trim()) { alert('Email address is required'); isValid = false; }
+    else if (!password.trim()) { alert('Password is required'); isValid = false; }
+    return isValid;
   }
 
-  render() {
+  getFormContent() {
+    const { firstName, emailAddress, password} = this.state;
     return (
       <Fragment>
         <div className='pre-title'>Let's</div>
         <div className='title'>Sign Up</div>
         <div className='verbiage'>Use the form below to sign up for this super awesome service. You're only a few steps away!</div>
         <div className='text-input-label'>First Name</div>
-        <input className='text-input' type='text' value={this.state.firstName} onChange={this.updateFirstName}></input>
+        <input className='text-input' type='text' value={firstName} onChange={this.updateFirstName}></input>
         <div className='text-input-label'>Email Address</div>
-        <input className='text-input' type='text' value={this.state.emailAddress} onChange={this.updateEmailAddress}></input>
+        <input className='text-input' type='text' value={emailAddress} onChange={this.updateEmailAddress}></input>
         <div className='text-input-label'>Password</div>
-        <input className='text-input' type='password' value={this.state.password} onChange={this.updatePassword}></input>
+        <input className='text-input' type='password' value={password} onChange={this.updatePassword}></input>
         <br/>
         <button onClick={this.signUp}>Sign Up</button>
       </Fragment>
     );
+  }
+
+  getConfirmationContent() {
+    return (
+      <Fragment>
+        Confirmation Content
+      </Fragment>
+    );
+  }
+
+  render() {
+    if (this.state.shouldShowConfirmation) return this.getConfirmationContent();
+    return this.getFormContent();
   }
 }
 
